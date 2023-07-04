67242a0a2fa25db29058377dc6d207d9195d8572
kpan2034 <keyur.panchal@gmail.com>
2023-07-04 13:08:02 -0400

    Fixed asi sql extension file.

--- asi--0.0.1.sql
+++ asi--0.0.1.sql
@@ -16,35 +16,32 @@ RETURNS boolean
 AS 'MODULE_PATHNAME', 'asi_try_intent_lock'
 LANGUAGE C VOLATILE STRICT;
 
-CREATE FUNCTION asi_try_table_exclusive_lock(integer, integer)
-RETURNS boolean
-AS 'MODULE_PATHNAME', 'asi_try_table_exclusive_lock'
-LANGUAGE C VOLATILE STRICT;
+-- CREATE FUNCTION asi_try_table_exclusive_lock(integer, integer)
+-- RETURNS boolean
+-- AS 'MODULE_PATHNAME', 'asi_try_table_exclusive_lock'
+-- LANGUAGE C VOLATILE STRICT;
+
+-- CREATE FUNCTION asi_try_table_shared_lock(integer, integer)
+-- RETURNS boolean
+-- AS 'MODULE_PATHNAME', 'asi_try_table_shared_lock'
+-- LANGUAGE C VOLATILE STRICT;
 
 CREATE FUNCTION asi_validate_intent_lock(integer, integer)
-RETURNS boolean
+RETURNS void 
 AS 'MODULE_PATHNAME', 'asi_validate_intent_lock'
 LANGUAGE C VOLATILE STRICT;
 
-CREATE FUNCTION asi_release_intent_lock(integer, integer)
-RETURNS void
-AS 'MODULE_PATHNAME', 'asi_release_intent_lock'
-LANGUAGE C VOLATILE STRICT;
+-- CREATE FUNCTION asi_release_intent_lock(integer, integer)
+-- RETURNS void
+-- AS 'MODULE_PATHNAME', 'asi_release_intent_lock'
+-- LANGUAGE C VOLATILE STRICT;
 
 CREATE FUNCTION asi_release_exclusive_lock(integer, integer)
 RETURNS void
 AS 'MODULE_PATHNAME', 'asi_release_exclusive_lock'
 LANGUAGE C VOLATILE STRICT;
 
-
-
-/**/
-/* -- contrib/asi/asi-0.0.1.sql */
-/**/
-/* -- complain if script is sourced in psql, rather than via ALTER EXTENSION */
-/* \echo Use "CREATE EXTENSION asi" to load this file. \quit */
-/**/
-/* CREATE FUNCTION slock(text) */
-/* RETURNS text */
-/* AS '$libdir/asi' */
-/* LANGUAGE C IMMUTABLE STRICT; */
+CREATE FUNCTION asi_release_shared_lock(integer, integer)
+RETURNS void
+AS 'MODULE_PATHNAME', 'asi_release_shared_lock'
+LANGUAGE C VOLATILE STRICT;
